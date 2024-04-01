c24daad4a7ae4c6e18deb0656c856d2e8db096cf
MauryIV <mauryhughesiv@gmail.com>
2024-04-01 17:38:01 -0600

    Added Comment to models appropriately.

--- models/index.js
+++ models/index.js
@@ -1,8 +1,18 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
   onDelete: 'CASCADE'
 });
 
@@ -10,4 +20,12 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blog, Comment };
